8815f7bcd1f0226e1bbf96b37850367b23fd1c2e
Pandu Yudhistira <panduyudhistira@Pandus-MacBook.local>
2023-03-29 23:23:54 +0700

    init

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import express,  {Application, Request, Response} from "express";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.APP_PORT || 3000;
+
+
+const configuration = new Configuration({
+    organization: "org-4sRnOLl7acH4A5m7fnM8q10W",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+app.get('/', async (req: Request, res: Response) => {
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.listEngines();
+    console.log(response);
+    return res.send('Halo Halo Bandung');
+});
+
+app.listen(port, () => {
+    return console.log(`App listening in port ${port}`);
+});
---
+++ src/lib/openai.ts
@@ -0,0 +1,10 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    organization: "org-4sRnOLl7acH4A5m7fnM8q10W",
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+
+console.log(response);
